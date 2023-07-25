33dc7aea0a57e1b64b3529aa44770aae888824c7
Michael Jansen <mjansen4857@gmail.com>
2023-07-25 12:09:32 -0400

    Initial commit

---
+++ example/nt4_example.dart
@@ -0,0 +1,6 @@
+import 'package:nt4/nt4.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/nt4_test.dart
@@ -0,0 +1,16 @@
+import 'package:nt4/nt4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
